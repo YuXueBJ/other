3e17a89d70002ceb10fbf44f5292849605e882e1
朱洪伟 <zhuhongwei@ZhuHongdeMacBook-Pro.local>
2014-07-21 12:26:05 +0800

    zhwProject

---
+++ zhwProject/zhwProject/ZWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZWAppDelegate.h
+//  zhwProject
+//
+//  Created by 朱洪伟 on 14-7-21.
+//  Copyright (c) 2014年 zhu hong wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zhwProject/zhwProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  zhwProject
+//
+//  Created by 朱洪伟 on 14-7-21.
+//  Copyright (c) 2014年 zhu hong wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZWAppDelegate class]));
+    }
+}
